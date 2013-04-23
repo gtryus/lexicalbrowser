a621ffb1313c5d54e7fdd47aa762f0fb88a0e93c
Mark <marksrcn@gmail.com>
2013-04-23 15:11:46 -0600

    Add Clear button

--- app.js
+++ app.js
@@ -35,7 +35,11 @@ App.SearchController = Ember.Controller.extend({
     
     results: function(){
         return search(this.get('query'))
-    }.property('query')
+    }.property('query'),
+
+    clear: function(){
+        this.set('query', '')
+    }
 });
 
 
