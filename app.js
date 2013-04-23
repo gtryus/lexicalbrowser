65d25ef69740e79864cd0b52bb2267c83353656f
Daniel Von Fange <daniel@leancoder.com>
2013-04-23 08:17:08 -0400

    Make search page use generated search results from the code.

--- app.js
+++ app.js
@@ -3,20 +3,42 @@ var FAKE_DATA = []
 App = Ember.Application.create();
 
 App.Router.map(function() {
-    this.route('search', {'path': '/search/:q'})
+    this.route('search', {'path': '/search/:query'})
 })
 
 
 App.SearchRoute = Ember.Route.extend({
-    model: function(){
-        return []
-    }
+    model: function(params){
+        return {query: params.query}
+    },
+    setupController: function(controller, model) {
+        controller.set('query', model.query);
+        controller.set('content', search(model.query));
+      }
 })
 
 
 App.IndexController = Ember.Controller.extend({
+    query: '',
     search: function(){
         this.transitionToRoute('search', this.get('query'));
     }
 });
 
+App.SearchController = Ember.Controller.extend({
+    query: '',
+    search: function(){
+        this.transitionToRoute('search', this.get('query'));
+    }
+});
+
+
+// Takes a query, and returns a list of results
+// Faked out for now
+function search(query){
+    return [
+        {'form':'Ixqupu', 'gloss': 'Test'},
+        {'form':'HzOoo', 'gloss': 'Running'},
+        {'form':'Lnxx', 'gloss': 'Cooking'},
+    ]
+}
